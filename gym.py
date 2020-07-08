c0fbfc4ba61533e7d002d1425dc65c995b35548d
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-07-08 00:11:04 -0700

    set up the genral gym framework

--- gym.py
+++ gym.py
@@ -2,9 +2,46 @@
 This script will take a put a model and dataset in an academy to train
 """
 # Imports
-from scripts.vanilla_nn import Vanilla_NN
-from scripts.academy import Academy
-import pandas as pd
+import os
+from data_setup import Data
+from vanilla_nn import Vanilla_NN
+from academy import Academy
+import torch
+
+# Hyperparameters
+csv_file = "data/weatherdata_merged_truncated_50.csv"
+n_epochs = 0
+gpu = False
+save_model = True
+
+
+# Push to GPU if necessary
+if gpu:
+    os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
+    os.environ["CUDA_VISIBLE_DEVICES"] = "1"
 
 # Load data
-data = pd.read_csv("data/weatherdata_merged_truncated_50.csv")
+data = Data(csv_file = csv_file, 
+            gpu = gpu)
+
+# Initalize network
+net = Vanilla_NN(input_size = data.n_features)
+
+# Enter student network and curriculum data into an academy
+academy  = Academy(net, data, gpu)
+
+# Fit Model
+academy.train(n_epochs = n_epochs,
+              batch_size = 5)
+
+# Calculate accuracy on test set
+test_loss = academy.test()
+print(test_loss)
+
+# Save Model
+if save_model:
+    # Define File Names
+    filename  = "net_w_loss_" + str(int(round(test_loss * 100, 3))) + ".pt"
+    
+    # Save Models
+    torch.save(academy.net.state_dict(), filename)
