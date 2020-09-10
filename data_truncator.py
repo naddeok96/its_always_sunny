79082c876ad3cbb1fb17ec1078a1744a552950c1
Kyle Naddeo <naddeok@gmail.com>
2020-09-10 00:36:00 -0400

    changed filename

--- data_truncator.py
+++ data_truncator.py
@@ -7,20 +7,21 @@ import pandas as pd
 # Hyperparameters
 truncation_size = 50
 save_truncd = True
-file_name = "weatherdata_merged"
+file_name = "weatherdata_merged2_training"
 
 # Load data
-data = pd.read_csv("../../data/Weather/" + file_name + ".csv")
+data = pd.read_csv("../data/Weather/" + file_name + ".csv")
 
 # Preview
 print(data.head())
 
-print("Datatypes")
-print(data.dtypes)
-
-# Create a small truncated version for hacking
-truncd_data = data[:truncation_size]
+# print("Datatypes")
+# print(data.dtypes)
 
 if save_truncd:
-    truncd_data.to_csv(file_name + "_truncated_" + str(truncation_size))
+    # Create a small truncated version for hacking
+    truncd_data = data[:truncation_size]
+    
+    # Save truncation
+    truncd_data.to_csv(file_name + "_truncated_" + str(truncation_size) + ".csv")
 
