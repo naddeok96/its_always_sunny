7eaa74dfca73a82e2b19ffecf887ee42ea7941b1
Kyle Naddeo <naddeok5@students.rowan.edu>
2020-10-22 15:03:03 -0700

    added autoencoder training function

--- data_loader.py
+++ data_loader.py
@@ -17,13 +17,12 @@ class WeatherDataset(torch.utils.data.Dataset):
         self.data = pd.read_csv(csv_file)
 
         # Temporary testbed cleans
-        # self.data.drop("Unnamed: 0", axis = 1, inplace = True) # This is just the index col saved by accident
+        self.data.drop("Unnamed: 0", axis = 1, inplace = True) # This is just the index col saved by accident
         self.data[self.data != self.data] = 0 # This is to convert NaN's to 0
-        self.data.drop(["year", "month", "day", "geoid"], axis = 1, inplace = True) # Remove categorical features
+        # self.data.drop(["year", "month", "day", "geoid"], axis = 1, inplace = True) # Remove categorical features
 
         # Transforms
         self.data = torch.tensor(self.data.values) # Convert to a Tensor
-            # Norm Data
 
     def __len__(self):
         return len(self.data)
--- data_setup.py
+++ data_setup.py
@@ -1,6 +1,8 @@
 # Imports
-from data_loader import WeatherDataset
 from torch.utils.data import DataLoader, random_split
+from data_loader import WeatherDataset
+import numpy as np
+import torch
 import math
 
 class Data:
@@ -8,15 +10,22 @@ class Data:
     def __init__(self,  csv_file,
                         gpu = False,
                         test_batch_size  = 100,
-                        train_percentage = 0.8):
+                        train_percentage = 0.8,
+                        category = None,
+                        one_hot_embedding_size = 32):
         """
         Args:
             csv_file (string): Relative path to the csv file
             gpu (bool, optional): If True use GPU's. Defaults to False.
             test_batch_size (int, optional): Size of batches in the test set. Defaults to 100.
             train_percentage (float, optional): Percentage of dataset to be allocated for training. Defaults to 0.8.
+            category(int, optional): Column to have an autoencoder dataset. 
+                                        # 1 is year
+                                        # 2 is month
+                                        # 3 is day
+                                        # 4 is geoid
+                                        # Defaults to None.
         """
-
         super(Data,self).__init__()
 
         # Hyperparameters
@@ -25,12 +34,30 @@ class Data:
 
         # Pull in data
         self.weather_dataset = WeatherDataset(csv_file = csv_file)
+
+        # Convert dataset for autoencoder
+        if category is not None:
+            # Initalize tensor to hold categorys data
+            categorical_data = torch.zeros(len(self.weather_dataset), dtype=torch.int32)
+
+            # Cycle through all data and store category data
+            for i, data in enumerate(self.weather_dataset):
+                categorical_data[i] = data[category].item() 
+
+            # Convert to right format for one hot function and reduce the size
+            categorical_data = categorical_data.type(torch.LongTensor) - min(categorical_data).item()
+
+            # One hot encode
+            self.weather_dataset = torch.nn.functional.one_hot(categorical_data, one_hot_embedding_size).type(torch.FloatTensor)
+
+        # Find size of feature space
         self.n_features = len(self.weather_dataset[0]) - 1
 
         # Split Data
-        train_portion = math.ceil(train_percentage * len(self.weather_dataset))
-        test_portion  = len(self.weather_dataset) - train_portion
-        self.train_set, self.test_set = random_split(self.weather_dataset.data, [train_portion, test_portion])
+        train_proportion = math.ceil(train_percentage * len(self.weather_dataset))
+        test_proportion  = len(self.weather_dataset) - train_proportion
+
+        self.train_set, self.test_set = random_split(self.weather_dataset.data, [train_proportion, test_proportion])
 
 
         #Test and validation loaders have constant batch sizes, so we can define them 
